afd6f7ad132e7fd01552e661864986f2ea56abde
Mr-burns <mr-burns@mr-burns-Nitro-ANV16-41>
2025-09-02 10:18:41 +0200

    add some func

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "prototypes.h"
+
+int main(void)
+{
+    char src[] = "Helloworld";
+    char dest[] = "";
+    ft_putstr("j'aimerais coder\n");
+    printf("%d", (ft_strlen(src)));
+    printf("\n%s", (ft_strcpy(dest, src)));
+}
---
+++ prototypes.h
@@ -0,0 +1,8 @@
+#ifndef LIBFT_H
+#define LIBFT_H
+
+char *ft_putstr(char *c);
+char *ft_strcpy(char *dest, char *src);
+int ft_strlen(char *str);
+
+#endif
