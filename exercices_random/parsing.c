b25c11088c7c7a2007ffe8f8035aa2e92491588f
Mr-burns <mr-burns@mr-burns-Nitro-ANV16-41>
2025-11-01 11:15:14 +0100

    gitignore

---
+++ exercices_random/parsing.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int ft_strlen(char *str)
+{
+	int i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+char parsing(char *f)
+{
+	int i = 0;
+
+}
+
+int main()
+{
+	FILE *f = fopen("parsing.txt", "r+");
+	if (f == NULL)
+	{
+		printf("Erreur d'ouverture du fichier");
+	}
+	printf("%c", f);
+	fclose(f);
+} 
